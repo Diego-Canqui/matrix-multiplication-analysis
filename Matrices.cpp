46fa384b749d291d1727ff117eee22df8c8667f3
Diego <dcanqui@unsa.edu.pe>
2026-05-02 10:04:18 -0500

    mide tiempo con chrono (aprendiendo a usarlo)

--- Matrices.cpp
+++ Matrices.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <chrono>
 
 using namespace std;
+using namespace chrono;
 
 //Recordando la primera vez que hice multiplicacion de matrices
 void multi_matriz_cuadrada(int A[4][4], int B[4][4], int C[4][4], int n) {  //Esto es n^3
@@ -16,8 +18,25 @@ void multi_matriz_cuadrada(int A[4][4], int B[4][4], int C[4][4], int n) {  //Es
 
 int main() {
 
-	cout << "hola mundo" << endl;
+    int n = 4; //variable del tamanio 
 
-	return 0;
+    int A[4][4], B[4][4], C[4][4];
 
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++) {
+            A[i][j] = 1;
+            B[i][j] = 1;
+        }
+
+    auto inicio = high_resolution_clock::now();
+
+    multi_matriz_cuadrada(A, B, C, n);
+
+    auto fin = high_resolution_clock::now();
+
+    auto duracion = duration_cast<microseconds>(fin - inicio);
+
+    cout << "Tiempo: " << duracion.count() << " microsegundos\n";
+
+    return 0;
 }
